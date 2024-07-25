73ffaea02b5f356e56e1d358ca5fff2cc47d861a
pini001206@163.com <pini001206@163.com>
2024-07-25 16:48:05 +0800

    修改包

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,22 +10,22 @@ const { token } = storeToRefs(useConfigStore())
 const { setToken } = useConfigStore()
 
 const calculate = () => {
-  userStore.increment()
-  setToken(String(userStore.count))
+    userStore.increment()
+    setToken(String(userStore.count))
 }
 
 </script>
 
 <template>
-  这是home 页面
-  <h1 class="number">{{ userStore.count }}</h1>
-  <h1 class="number">{{ token }}</h1>
-  <el-button type="primary" @click="calculate()">计算count</el-button>
+    这是home 页面
+    <h1 class="number">{{ userStore.count }}</h1>
+    <h1 class="number">{{ token }}</h1>
+    <button @click="calculate()">计算count</button>
 </template>
 
 <style lang="less" scoped>
 .number {
-  color: blue;
-  margin-left: 50px;
+    color: blue;
+    margin-left: 50px;
 }
 </style>
